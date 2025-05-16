02cc0db528fc34596246e615a864ffaaebb8d652
Panczel24 <pcsenge8@gmail.com>
2025-05-16 10:15:46 +0200

    Kérdések listába téve/kérdések class

---
+++ LegyenÖnIsMilliomos/Kerdes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegyenÖnIsMilliomos
+{
+    internal class Kerdes
+    {
+        List<string> kerdesvalaszok = new List<string>();
+        string kerdes;
+        string megoldas;
+        string kategoria;
+        public Kerdes(string kerdes, string valasz1, string valasz2, string valasz3, string valasz4, string megoldas, string kategoria)
+        {
+            this.kerdes = kerdes;
+            this.kerdesvalaszok.Add(valasz1);
+            this.kerdesvalaszok.Add(valasz2);
+            this.kerdesvalaszok.Add(valasz3);
+            this.kerdesvalaszok.Add(valasz4);
+
+            this.megoldas = megoldas;
+            this.kategoria = kategoria;
+        }
+
+        public string Kerdesvalaszok { get => kerdes; set => kerdes = value; }
+
+
+        public string Megoldas { get => megoldas; set => megoldas = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+        public List<string> Valaszok { get => kerdesvalaszok; set => kerdesvalaszok = value; }
+
+
+
+
+
+
+        public override string? ToString()
+        {
+            return $"Kérdés: {kerdes}\n" +
+                $"A: {kerdesvalaszok[0],-30} B: {kerdesvalaszok[1],-30}\n" +
+                $"C: {kerdesvalaszok[2],-30} D: {kerdesvalaszok[3],-30}\n" +
+                $"Kategória: {kategoria}, Megoldás: {megoldas}";
+        }
+    }
+}
+}
