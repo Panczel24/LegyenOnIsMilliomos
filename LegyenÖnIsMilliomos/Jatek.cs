c2b107a973bf433e72825a9aeccd7618a31920ad
Panczel24 <pcsenge8@gmail.com>
2025-05-16 20:19:44 +0200

    Segítségek

--- LegyenÖnIsMilliomos/Jatek.cs
+++ LegyenÖnIsMilliomos/Jatek.cs
@@ -47,71 +47,248 @@ namespace LegyenÖnIsMilliomos
 
                 for (int i = 0; i < Kerdessor.KerdesCsoport.Count; i++)
                 {
-                   var aktualisCsoport = Kerdessor.KerdesCsoport[i];
+                    var aktualisCsoport = Kerdessor.KerdesCsoport[i];
                     int veletlenIndex = r.Next(aktualisCsoport.Count);
                     Kerdes kerdes = aktualisCsoport[veletlenIndex];
 
                     Console.WriteLine($"\n{i + 1}. kérdés");
                     Console.WriteLine(kerdes);
-                    Console.WriteLine("Kér segítséget? (nem, felező, telefon, közönség)");
+                    Console.WriteLine("Kér segítséget? (nem, " + string.Join(", ", segitsegTipusok) + ")");
                     string kere = Console.ReadLine().ToLower();
-                    switch (kere)
+                    if (kere == "nem")
                     {
-                        case "nem":
-                            break;
-                    }
+                        Console.Write("Adja meg a jó választ (A, B, C, D): ");
+                        string valasz = Console.ReadLine().ToUpper();
 
+                        if (valasz == kerdes.Megoldas.ToUpper())
+                        {
+                            Console.WriteLine("Helyes válasz!");
+                            penz += 100000;
+                            Console.WriteLine($"Eddig ennyi pénzt szereztél:{penz}");
+                            if (penz != biz3 && penz != biz6 && penz != biz9 && penz != biz12)
+                            {
+                                hang.Effektus("jo.wav");
+                            }
+                            if (penz == biz3 || penz == biz6 || penz == biz9 || penz == biz12)
+                            {
+                                hang.Effektus("biztos.wav");
+                                Console.WriteLine("Gratulálok, egy biztos nyereményhez értél!");
+                                Thread.Sleep(4000);
+                            }
+                            if (penz == biz3)
+                            {
+                                biztosmegvan = biz3;
+                            }
+                            if (penz == biz6)
+                            {
+                                biztosmegvan = biz6;
+                            }
+                            if (penz == biz9)
+                            {
+                                biztosmegvan = biz9;
+                            }
+                            if (penz == biz12)
+                            {
+                                biztosmegvan = biz12;
+                            }
+                            if (penz == max)
+                            {
+                                Console.WriteLine("Gratulálok, megnyerte az egymillió-ötszázezer forintot!");
+                                hang.Leallitas();
+                                hang.Effektus("vege.wav");
+                            }
 
-                    Console.Write("Adja meg a jó választ (A, B, C, D): ");
-                    string valasz = Console.ReadLine().ToUpper();
-
-                    if (valasz == kerdes.Megoldas.ToUpper())
-                    {
-                        Console.WriteLine("Helyes válasz!");
-                        penz += 100000;
-                        Console.WriteLine($"Eddig ennyi pénzt szereztél:{penz}");
-                        if (penz != biz3 && penz != biz6 && penz != biz9 && penz != biz12)
-                        {
-                            hang.Effektus("jo.wav");
                         }
-                        if (penz == biz3 || penz == biz6 || penz == biz9 || penz == biz12)
+                        else
                         {
-                            hang.Effektus("biztos.wav");
-                            Console.WriteLine("Gratulálok, egy biztos nyereményhez értél!");
-                            Thread.Sleep(4000);
-                        }
-                        if (penz == biz3)
-                        {
-                            biztosmegvan = biz3;
-                        }
-                        if (penz == biz6)
-                        {
-                            biztosmegvan = biz6;
-                        }
-                        if (penz == biz9)
-                        {
-                            biztosmegvan = biz9;
-                        }
-                        if (penz == biz12)
-                        {
-                            biztosmegvan = biz12;
-                        }
-                        if (penz == max)
-                        {
-                            Console.WriteLine("Gratulálok, megnyerte az egymillió-ötszázezer forintot!");
+                            Console.WriteLine($"Sajnos rossz választ adtál, a játékot nem folytathatod" +
+                                $"\nA biztos nyereményed:{biztosmegvan}");
+                            hang.Effektus("rossz.wav");
+                            Thread.Sleep(2000);
                             hang.Leallitas();
-                            hang.Effektus("vege.wav");
+                            return;
+                        }
+                    }
+                    else if (kere == "felező")
+                    {
+                        var felezoValaszok = Segitseg.Felezo(kerdes);
+                        segitsegTipusok.Remove("felező");
+                        Console.WriteLine("Felező segítség aktiválva! Két válaszlehetőség maradt:");
+
+                        foreach (var felezovalasz in felezoValaszok)
+                        {
+                            Console.WriteLine(felezovalasz);
                         }
 
+                        Console.Write($"Add meg a válaszod: ");
+                        string valasz = Console.ReadLine().ToUpper();
+
+                        if (valasz == kerdes.Megoldas.ToUpper())
+                        {
+                            Console.WriteLine("Helyes válasz!");
+                            penz += 100000;
+                            Console.WriteLine($"Eddig ennyi pénzt szereztél:{penz}");
+                            if (penz != biz3 && penz != biz6 && penz != biz9 && penz != biz12)
+                            {
+                                hang.Effektus("jo.wav");
+                            }
+                            if (penz == biz3 || penz == biz6 || penz == biz9 || penz == biz12)
+                            {
+                                hang.Effektus("biztos.wav");
+                                Console.WriteLine("Gratulálok, egy biztos nyereményhez értél!");
+                                Thread.Sleep(4000);
+                            }
+                            if (penz == biz3)
+                            {
+                                biztosmegvan = biz3;
+                            }
+                            if (penz == biz6)
+                            {
+                                biztosmegvan = biz6;
+                            }
+                            if (penz == biz9)
+                            {
+                                biztosmegvan = biz9;
+                            }
+                            if (penz == biz12)
+                            {
+                                biztosmegvan = biz12;
+                            }
+                            if (penz == max)
+                            {
+                                Console.WriteLine("Gratulálok, megnyerte az egymillió-ötszázezer forintot!");
+                                hang.Leallitas();
+                                hang.Effektus("vege.wav");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sajnos rossz választ adtál, a játékot nem folytathatod" +
+                                $"\nA biztos nyereményed:{biztosmegvan}");
+                            hang.Effektus("rossz.wav");
+                            Thread.Sleep(2000);
+                            hang.Leallitas();
+                            return;
+                        }
                     }
-                    else
+                    else if (kere == "telefon")
                     {
-                        Console.WriteLine($"Sajnos rossz választ adtál, a játékot nem folytathatod" +
-                            $"\nA biztos nyereményed:{biztosmegvan}");
-                        hang.Effektus("rossz.wav");
-                        Thread.Sleep(2000);
-                        hang.Leallitas();
-                        return;
+                        segitsegTipusok.Remove("telefon");
+                        Console.WriteLine("Telefonos segítség aktiválva....*telefonbeszélgetés*");
+                        Console.WriteLine(Segitseg.Telefon(kerdes));
+                        Console.Write("Add meg a válaszod: ");
+                        string valasz = Console.ReadLine().ToUpper();
+
+                        if (valasz == kerdes.Megoldas.ToUpper())
+                        {
+                            Console.WriteLine("Helyes válasz!");
+                            penz += 100000;
+                            Console.WriteLine($"Eddig ennyi pénzt szereztél:{penz}");
+                            if (penz != biz3 && penz != biz6 && penz != biz9 && penz != biz12)
+                            {
+                                hang.Effektus("jo.wav");
+                            }
+                            if (penz == biz3 || penz == biz6 || penz == biz9 || penz == biz12)
+                            {
+                                hang.Effektus("biztos.wav");
+                                Console.WriteLine("Gratulálok, egy biztos nyereményhez értél!");
+                                Thread.Sleep(4000);
+                            }
+                            if (penz == biz3)
+                            {
+                                biztosmegvan = biz3;
+                            }
+                            if (penz == biz6)
+                            {
+                                biztosmegvan = biz6;
+                            }
+                            if (penz == biz9)
+                            {
+                                biztosmegvan = biz9;
+                            }
+                            if (penz == biz12)
+                            {
+                                biztosmegvan = biz12;
+                            }
+                            if (penz == max)
+                            {
+                                Console.WriteLine("Gratulálok, megnyerte az egymillió-ötszázezer forintot!");
+                                hang.Leallitas();
+                                hang.Effektus("vege.wav");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sajnos rossz választ adtál, a játékot nem folytathatod" +
+                                $"\nA biztos nyereményed:{biztosmegvan}");
+                            hang.Effektus("rossz.wav");
+                            Thread.Sleep(2000);
+                            hang.Leallitas();
+                            return;
+                        }
+                    }
+                    else if (kere == "közönség")
+                    {
+                        segitsegTipusok.Remove("közönség");
+                        Console.WriteLine("Közönség segítsége....*nézők szavaznak*");
+                        Console.WriteLine(Segitseg.Kozonseg(kerdes));
+                        Console.Write("Add meg a válaszod: ");
+                        string valasz = Console.ReadLine().ToUpper();
+
+                        if (valasz == kerdes.Megoldas.ToUpper())
+                        {
+                            Console.WriteLine("Helyes válasz!");
+                            penz += 100000;
+                            Console.WriteLine($"Eddig ennyi pénzt szereztél:{penz}");
+                            if (penz != biz3 && penz != biz6 && penz != biz9 && penz != biz12)
+                            {
+                                hang.Effektus("jo.wav");
+                            }
+                            if (penz == biz3 || penz == biz6 || penz == biz9 || penz == biz12)
+                            {
+                                hang.Effektus("biztos.wav");
+                                Console.WriteLine("Gratulálok, egy biztos nyereményhez értél!");
+                                Thread.Sleep(4000);
+                            }
+                            if (penz == biz3)
+                            {
+                                biztosmegvan = biz3;
+                            }
+                            if (penz == biz6)
+                            {
+                                biztosmegvan = biz6;
+                            }
+                            if (penz == biz9)
+                            {
+                                biztosmegvan = biz9;
+                            }
+                            if (penz == biz12)
+                            {
+                                biztosmegvan = biz12;
+                            }
+                            if (penz == max)
+                            {
+                                Console.WriteLine("Gratulálok, megnyerte az egymillió-ötszázezer forintot!");
+                                hang.Leallitas();
+                                hang.Effektus("vege.wav");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sajnos rossz választ adtál, a játékot nem folytathatod" +
+                                $"\nA biztos nyereményed:{biztosmegvan}");
+                            hang.Effektus("rossz.wav");
+                            Thread.Sleep(2000);
+                            hang.Leallitas();
+                            return;
+                        }
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Ilyet vagy elhasználtál, vagy elírtál valamit");                        
                     }
                 }
             }
---
+++ LegyenÖnIsMilliomos/Segitseg.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegyenÖnIsMilliomos
+{
+    internal static class Segitseg
+    {
+        public static List<string> Felezo(Kerdes kerdes)
+        {
+            char helyesBetu = kerdes.Megoldas.ToUpper()[0];
+            int helyesIndex = 0;
+
+            if (helyesBetu == 'A') helyesIndex = 0;
+            else if (helyesBetu == 'B') helyesIndex = 1;
+            else if (helyesBetu == 'C') helyesIndex = 2;
+            else if (helyesBetu == 'D') helyesIndex = 3;
+
+            List<int> rosszIndexek = new List<int> { 0, 1, 2, 3 };
+            rosszIndexek.Remove(helyesIndex);
+
+            Random r = new Random();
+            int rosszIndex = rosszIndexek[r.Next(rosszIndexek.Count)];
+
+            string[] betuk = { "A", "B", "C", "D" };
+            return new List<string>
+            {
+                $"{betuk[helyesIndex]}: {kerdes.Valaszok[helyesIndex]}",
+                $"{betuk[rosszIndex]}: {kerdes.Valaszok[rosszIndex]}"
+                
+            };
+        }
+
+        public static string Telefon(Kerdes kerdes)
+        {
+            char helyesBetu = kerdes.Megoldas.ToUpper()[0];
+            int helyesIndex = 0;
+
+            if (helyesBetu == 'A') helyesIndex = 0;
+            else if (helyesBetu == 'B') helyesIndex = 1;
+            else if (helyesBetu == 'C') helyesIndex = 2;
+            else if (helyesBetu == 'D') helyesIndex = 3;
+
+            Random r = new Random();
+            int valasztottIndex;
+
+            if (r.NextDouble() < 0.6)
+            {
+                valasztottIndex = helyesIndex;
+            }
+            else
+            {
+                List<int> rosszIndexek = new List<int> { 0, 1, 2, 3 };
+                rosszIndexek.Remove(helyesIndex);
+                valasztottIndex = rosszIndexek[r.Next(rosszIndexek.Count)];
+            }
+
+            string[] betuk = { "A", "B", "C", "D" };
+            return $"Szerintem a helyes válasz: {betuk[valasztottIndex]} - {kerdes.Valaszok[valasztottIndex]}";
+
+        }
+
+        public static string Kozonseg(Kerdes kerdes)
+        {
+            char helyesBetu = kerdes.Megoldas.ToUpper()[0];
+            int helyesIndex = 0;
+
+            if (helyesBetu == 'A') helyesIndex = 0;
+            else if (helyesBetu == 'B') helyesIndex = 1;
+            else if (helyesBetu == 'C') helyesIndex = 2;
+            else if (helyesBetu == 'D') helyesIndex = 3;
+
+            Random r = new Random();
+            int valasztottIndex;
+
+            if (r.NextDouble() < 0.8)
+            {
+                valasztottIndex = helyesIndex;
+            }
+            else
+            {
+                List<int> rosszIndexek = new List<int> { 0, 1, 2, 3 };
+                rosszIndexek.Remove(helyesIndex);
+                valasztottIndex = rosszIndexek[r.Next(rosszIndexek.Count)];
+            }
+
+            string[] betuk = { "A", "B", "C", "D" };
+            return $"A nézők szerint a szerint a helyes válasz: {betuk[valasztottIndex]} - {kerdes.Valaszok[valasztottIndex]}";
+
+        }
+
+
+    }
+}
